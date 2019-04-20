b5a4c0a32aabf6a259dcbee073ccbc7edcf71cea
Sebastian Pinto B <sebastianpintobecerra7@gmail.com>
2019-04-20 18:21:21 -0500

    Se agrego el inicio de la aplicacion, se ataron la sala de ensayo 'Cueva de la cebra' con sus caracteristicas y salas disponibles.

--- build/web/js/cliente.js
+++ build/web/js/cliente.js
@@ -1,19 +1,8 @@
 $(document).ready(inicio);
 
 function inicio() {
-    //INICIO LOCALIDADES
-
-
-    //PRUEBA CALENDARIO ---------------------------------------------------------------------------------------------
-    //calendarioReservas();
-    $("#btnBuscarRangoCalendario").click(busquedaRangoFechasCalendario);
-    $("#anteriorFechaCalendario").click(function () {
-        alert("anteriorFechaCalendario");
-    });
-    $("#siguienteFechaCalendario").click(function () {
-        alert("siguienteFechaCalendario");
-    });
-
+    $(this).scrollTop(0);
+    obtenerMiPosicion();
 
     //OPCIONES DE USUARIO ---------------------------------------------------------------------------------------------
     $("#btnIniciarSesion").click(clickInicioSesion);
@@ -28,7 +17,6 @@ function inicio() {
         innerModalInformativo("configuracionUsuario", "configuracionUsuario", "configuracionUsuario", false);
     });
 
-
     //REGISTRO DE USUARIO ---------------------------------------------------------------------------------------------
     $("#registroUsuario").click(function () {
         $("#loginModal").modal('hide');
@@ -36,20 +24,33 @@ function inicio() {
     });
     $("#btnRegistrarUsuario").click(clickRegistroUsuario);
 
-    //CONTACTENOS ------------------------------------------------------------------------------------------------------
-    $("#pagContactenosClick").click(function () {
-        $("#pagInicio").hide();
-        $("#pagContactenos").show();
+    // MAPA ----------------------------------------------------------------------------------------------------------
+    $("#mapaRiff").click(accesoMapaRiff);
+
+    // LISTADO SALAS DE ENSAYO ----------------------------------------------------------------------------------------------------------
+    $("#listadoSalasEnsayo").click(accesolistadoSalasEnsayo);
+
+    //PRUEBA CALENDARIO ---------------------------------------------------------------------------------------------
+    //calendarioReservas();
+    $("#btnBuscarRangoCalendario").click(busquedaRangoFechasCalendario);
+    $("#anteriorFechaCalendario").click(function () {
+        alert("anteriorFechaCalendario");
     });
+    $("#siguienteFechaCalendario").click(function () {
+        alert("siguienteFechaCalendario");
+    });
+
+    //CONTACTENOS ------------------------------------------------------------------------------------------------------
+    $("#pagContactenosAcceso").click(accesoContactenos);
     $("#btnEnviarDatosContactenos").click(clickContactoUsuarioApp);
 
     //EVALUO DE LA SESION ACTIVA 
     if (localStorage.getItem("idSession") !== null) {
         skip();
+        accesoMapaRiff();
         usuarioSesionIniciada(dataUsuario.usuario);
-        $("#cuevaDeLaCebra").show();
     } else {
-        skip();
+        skipInicio();
     }
 }
 
@@ -156,22 +157,38 @@ function clickContactoUsuarioApp() {
     });
 }
 
-
 // FUNCIONES GENERALES -------------------------------------------------
 function skipInicio() {
+    $("#img-div-inicio").hide();
+    $("#bienvenido-div-inicio").hide();
+    $("#img-div-inicio").fadeIn(3000);
+    $("#bienvenido-div-inicio").fadeIn(3000);
+    $("#container-text-inicio").hide();
+    $("#container-text-inicio").delay(1000).slideDown("slow");
+
     $("#footerAplicacion").hide();
     $("#mainNav").hide();
-    $(".content-wrapper").hide();
-    $("#inicioLocalidades").show();
+    $("#cuerpoPaginaRiff").hide();
+
+    $("#inicioRiff").show();
 }
 function skip() {
-    $("#inicioLocalidades").hide();
+    $("#footerAplicacion").show();
+    $("#mainNav").show();
+    $("#cuerpoPaginaRiff").show();
+
+    $("#inicioRiff").hide();
+    $("#pagInicio").hide();
+    $("#cardsSitios").hide();
     $("#pagContactenos").hide();
+    //SITIOS
+    $("#pagSitios").hide();
+    $("#cuevaDeLaCebra").hide();
+
     $("#pagCalendarioPrueba").hide();
-//    $("#pagSitios").hide();
-//    $("#cuevaDeLaCebra").hide();
-//    $("#cardsSitios").hide();
-//    $("#cardsSalas").hide();
+    $("#cardsSalas").hide();
+
+    $("#pagMapa").show();
 }
 function usuarioSesionIniciada() {
     $("#usuarioSesionIniciada").removeAttr("hidden");
@@ -181,6 +198,7 @@ function usuarioSesionIniciada() {
             '<span class="d-lg-none">' +
             '<span class="badge badge-pill badge-primary"></span>' +
             '</span>');
+    $("#loginModal").modal('hide');
 }
 function innerModalInformativo(header, body, footer, xlModal) {
     //HEADER
@@ -202,9 +220,6 @@ function innerModalInformativo(header, body, footer, xlModal) {
     }
 
     $("#modalInformativo").modal('show');
-}
-function localidadSelect(id) {
-    alert(id);
 }
 
 // CALENDARIO ---------------------------------------------------------------------------------------------
@@ -261,6 +276,186 @@ function busquedaRangoFechasCalendario() {
 }
 
 // ACCESO ---------------------------------------------------------------------------------------------
-function accesoSalasDeEnsayo() {
+function accesoInicio() {
+    cargarMapa();
+    skip();
+    $("#pagMapa").show();
+}
+function accesoMapaRiff() {
+    accesoInicio();
+}
+function accesoSitios(id, ubicacion) {
+    skip();
+    //Limpia el breadcrumb
+    document.getElementById("breadcrumbSitios").innerHTML = null;
+    //Agrega un breadcrumb personalizado
+    switch (ubicacion) {
+        case "mapa":
+            $("#breadcrumbSitios").append(" <li class='breadcrumb-item'>" +
+                    "<a href='#' onclick='accesoInicio()'>Inicio</a>" +
+                    "</li>" +
+                    "<li class='breadcrumb-item' ><a class='breadcrumbSegundoNivel' onclick='accesoMapaRiff()'>Mapa</a></li>" +
+                    "<li class='breadcrumb-item active'>La Cueva de la cebra</li>");
+            break;
+        case "cardListado":
+            $("#breadcrumbSitios").append(" <li class='breadcrumb-item'>" +
+                    "<a href='index.html'>Inicio</a>" +
+                    "</li>" +
+                    "<li class='breadcrumb-item' ><a class='breadcrumbSegundoNivel' onclick='accesolistadoSalasEnsayo()'>Listado Sitios</a></li>" +
+                    "<li class='breadcrumb-item active'>La Cueva de la cebra</li>");
+            break;
+    }
+    $("#pagMapa").hide();
+    $("#" + id).show();
+    $("#pagSitios").show();
+}
+function accesoSalas(id, ubicacion) {
+    $(this).scrollTop(0);
+    skip();
+    //Limpia el breadcrumb
+    document.getElementById("breadcrumbSalas").innerHTML = null;
+    //Agrega un breadcrumb personalizado
+    $("#breadcrumbSalas").append(" <li class='breadcrumb-item'>" +
+            "<a href='#' onClick='accesoInicio();'>Inicio</a>" +
+            "</li>" +
+            "<li class='breadcrumb-item' ><a class='breadcrumbSegundoNivel' onclick='accesoSitios('cuevaDeLaCebra', 'mapa')'>La Cueva de la cebra</a></li>" +
+            "<li class='breadcrumb-item active'>Salas de ensayo</li>");
+    $("#pagMapa").hide();
+    $("#cardsSalas").show();
+    $("#" + id).show();
+}
+function accesolistadoSalasEnsayo() {
+    skip();
+    calcularDistancias();
+    $("#pagMapa").hide();
+    $("#cardsSitios").show();
+}
+function accesoContactenos() {
+    skip();
+    $("#pagMapa").hide();
+    $("#pagContactenos").show();
+}
 
+// MAPA ------------------------------------------------------------------------------------------------
+function cargarMapa() {
+    var x = document.getElementById("demo");
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    }
+
+    function showPosition(position) {
+//        var lat = position.coords.latitude;
+//        var lon = position.coords.longitude;
+        //Posicion en Corferias
+        var lat = 4.630132;
+        var lon = -74.089984;
+        latlon = new google.maps.LatLng(lat, lon);
+        mapholder = document.getElementById('mapholder');
+        mapholder.style.height = 'auto';
+        mapholder.style.width = '100%';
+        var myOptions = {
+            center: latlon, zoom: 15,
+            mapTypeId: google.maps.MapTypeId.ROADMAP,
+            mapTypeControl: false,
+            navigationControlOptions: {style: google.maps.NavigationControlStyle.SMALL}
+        };
+        var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+        var marker = new google.maps.Marker({
+            position: latlon,
+            map: map,
+            title: "Usted esta aqui"
+        });
+//        var popup = new google.maps.InfoWindow({content: 'Posicion Actual'});
+//        popup.open(map, marker);
+        marker.addListener('click', function canchacall() {
+
+        });
+
+        //MARCADO PARA CADA SALA
+        var place = new google.maps.LatLng(4.632132, -74.099984);
+        var marker1 = new google.maps.Marker({
+            position: place
+//            , title: 'La cueva de la Cebra'
+            , map: map
+            , icon: src = "imagenes/Logo/marker_reserv50_50.png"
+        });
+        var popup = new google.maps.InfoWindow({content: 'La Cueva de la Cebra'});
+        popup.open(map, marker1);
+        marker1.addListener('click', function () {
+            accesoSitios("cuevaDeLaCebra", "mapa");
+        });
+    }
+
+    function showError(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                x.innerHTML = "Denegada la peticion de Geolocalización en el navegador."
+                break;
+            case error.POSITION_UNAVAILABLE:
+                x.innerHTML = "La información de la localización no esta disponible."
+                break;
+            case error.TIMEOUT:
+                x.innerHTML = "El tiempo de petición ha expirado."
+                break;
+            case error.UNKNOWN_ERROR:
+                x.innerHTML = "Ha ocurrido un error desconocido."
+                break;
+        }
+    }
+}
+function obtenerMiPosicion() {
+    let vecPosicion = {
+        latitud: 0,
+        longitud: 0
+    };
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            vecPosicion.latitud = position.coords.latitude;
+            vecPosicion.longitud = position.coords.longitude;
+            localStorage.setItem("PosicionUser", JSON.stringify(vecPosicion));
+        });
+    }
+
+}
+function distanciaKMCoordenadasTierra(lat1, lon1, lat2, lon2) {
+    var earthRadiusKm = 6371;
+    var dLat = degreesToRadians(lat2 - lat1);
+    var dLon = degreesToRadians(lon2 - lon1);
+
+    lat1 = degreesToRadians(lat1);
+    lat2 = degreesToRadians(lat2);
+
+    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+            Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+
+    function degreesToRadians(degrees) {
+        return degrees * Math.PI / 180;
+    }
+    return earthRadiusKm * c;
+}
+
+// CARD SITIOS -----------------------------------------------------------------------------------------
+function calcularDistancias() {
+    let posicionCueva = [4.632132, -74.099984];
+    let posicionPakarny = [4.624101, -74.074366];
+    let posicionElToke = [4.689083, -74.061597];
+
+    //UBICACION CORFERIAS
+    let lat = 4.630132;
+    let lon = -74.089984;
+//    let lat = JSON.parse(localStorage.getItem("PosicionUser")).latitud;
+//    let lon = JSON.parse(localStorage.getItem("PosicionUser")).longitud;
+// DISTANCIA AL A CUEVA
+    document.getElementById("distanciaUsuarioCueva").innerHTML = "A <b>" +
+            distanciaKMCoordenadasTierra(lat, lon, posicionCueva[0], posicionCueva[1]).toFixed(2) +
+            " KM</b>, de su posición aproximadamente.";
+    //DISTANCIA A PAKARNY
+    document.getElementById("distanciaUsuarioPakarny").innerHTML = "A <b>" +
+            distanciaKMCoordenadasTierra(lat, lon, posicionPakarny[0], posicionPakarny[1]).toFixed(2) +
+            " KM</b>, de su posición aproximadamente.";
+    //DISTANCIA AL TOKE
+    document.getElementById("distanciaUsuarioElToke").innerHTML = "A <b>" +
+            distanciaKMCoordenadasTierra(lat, lon, posicionElToke[0], posicionElToke[1]).toFixed(2) +
+            " KM</b>, de su posición aproximadamente.";
 }
